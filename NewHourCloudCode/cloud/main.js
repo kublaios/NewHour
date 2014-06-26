ed5c8a2f12aa36f3ba06b52674c4047b066185fa
Kubilay Erdogan Mac Mini <kublaios@gmail.com>
2014-06-26 11:35:47 +0300

    Time is not dynamic.

--- NewHourCloudCode/cloud/main.js
+++ NewHourCloudCode/cloud/main.js
@@ -7,7 +7,14 @@ Parse.Cloud.define("hello", function(request, response) {
 
 // let's create a function that returns to time with a salutation
 Parse.Cloud.define("sayMyName", function(request, response) {
-	var theTime = "10:00 am";
+	// get hour and minutes
+	var date = new Date();
+	var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+    // create the time and response message
+	var theTime = hour + ":" + min;
 	var msg = "Hello, " + request.params.myName + "! The time is " + theTime + ".";
 	response.success(msg);
 });
