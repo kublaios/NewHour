dd7f92bfc30c3379dc8eec25d588410e24093350
Kubilay Erdogan Mac Mini <kublaios@gmail.com>
2014-06-26 09:52:04 +0300

    * Initial commit of Xcode project with proper .gitignore and ignoring the Parse Keys * If you want to set your own Parse keys, create a ParseKeys.plist and then insert all keys into this file. * Remember to ignore that file, too. * Sample .gitignore file: https://gist.github.com/kublaios/0f4a19bd0517a7fba08b

---
+++ NewHour/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NewHour
+//
+//  Created by Kubilay Erdogan on 26/06/14.
+//  Copyright (c) 2014 kublaios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NewHour/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NewHour
+//
+//  Created by Kubilay Erdogan on 26/06/14.
+//  Copyright (c) 2014 kublaios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
