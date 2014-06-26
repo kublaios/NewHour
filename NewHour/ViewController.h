70bbd52b5e7e325096b4b43ef506d5150108dfbe
Kubilay Erdogan Mac Mini <kublaios@gmail.com>
2014-06-26 11:26:19 +0300

    Call cloud function and show it in a text view.

--- NewHour/ViewController.h
+++ NewHour/ViewController.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    UITextView *txtResponse;
+}
 
 @end
--- NewHour/ViewController.m
+++ NewHour/ViewController.m
@@ -18,6 +18,19 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // create the text view to see the response
+    txtResponse = [[UITextView alloc] initWithFrame:CGRectMake(50, 100, CGRectGetWidth(self.view.frame) - 00, CGRectGetHeight(self.view.frame) - 200)];
+    [self.view addSubview:txtResponse];
+    
+    // get the response from the server and show it in the textview
+    [PFCloud callFunctionInBackground:@"hello" withParameters:@{} block:^(NSString *result, NSError *error) {
+        if (error) {
+            txtResponse.text = error.localizedDescription;
+        } else {
+            txtResponse.text = result;
+        }
+    }];
 }
 
 - (void)didReceiveMemoryWarning
